ce4cd301dc4663ea386c652fa22907c1e3ce42a5
dpetrov <daniel.petrov@tick42.com>
2017-02-20 01:21:21 +0200

    logic updated to support api functions with multiple params, half of the api functions added

--- src/AgGridApi.js
+++ src/AgGridApi.js
@@ -1,10 +1,5 @@
 import React, { PureComponent as Component } from 'react'
-
-const ALL_AG_GRID_API_FUNCTIONS = [
-    'exportDataAsCsv',
-    'selectAll',
-    'deselectAll'
-]
+import { ALL_AG_GRID_API_FUNCTIONS } from './constants'
 
 const DEFAULT_OPTIONS = {
     log: false,
@@ -23,13 +18,17 @@ export const AgGridApi = (DecoratedComponent, options = DEFAULT_OPTIONS) => {
             this.state.isGridReady = false
         }
 
-        afterGridReady = (callback, options) => {
+        afterGridReady = () => {
+            const callback = arguments[0]
+            const options = Array.prototype.slice.call(arguments, 1)
             const { isGridReady, gridParams } = this.state
 
             if (isGridReady) {
-               return callback.call(gridParams.api, options)
+               return callback.call(gridParams.api, ...options)
             } else if (options.log) {
                 console.warn('Ag grid has not loaded yet')
+
+                return null
             }
         }
 
@@ -44,14 +43,143 @@ export const AgGridApi = (DecoratedComponent, options = DEFAULT_OPTIONS) => {
             })
         }
 
-        exportDataAsCsv = () => {
-            const { gridParams } = this.state
+        setRowData = rows => {
+            this.afterGridReady(this.gridParamsFunctionSelector('setRowData'), rows)
+        }
 
-            this.afterGridReady(this.gridParamsFunctionSelector('exportDataAsCsv'), gridParams)
+        setDatasource = datasource => {
+            this.afterGridReady(this.gridParamsFunctionSelector('setDatasource'), datasource)
+        }
+
+        setColumnDefs = colDefs => {
+            this.afterGridReady(this.gridParamsFunctionSelector('setColumnDefs'), colDefs)
+        }
+
+        sizeColumnsToFit = () => {
+            this.afterGridReady(this.gridParamsFunctionSelector('sizeColumnsToFit'))
         }
 
         selectAll = () => {
             this.afterGridReady(this.gridParamsFunctionSelector('selectAll'))
+        }
+
+        deselectAll = () => {
+            this.afterGridReady(this.gridParamsFunctionSelector('deselectAll'))
+        }
+
+        selectAllFiltered = () => {
+            this.afterGridReady(this.gridParamsFunctionSelector('selectAllFiltered'))
+        }
+
+        deselectAllFiltered = () => {
+            this.afterGridReady(this.gridParamsFunctionSelector('deselectAllFiltered'))
+        }
+
+        getSelectedNodes = () => {
+            return this.afterGridReady(this.gridParamsFunctionSelector('getSelectedNodes'))
+        }
+
+        getSelectedRows = () => {
+            return this.afterGridReady(this.gridParamsFunctionSelector('getSelectedRows'))
+        }
+
+        getBestCostNodeSelection = () => {
+            return this.afterGridReady(this.gridParamsFunctionSelector('getBestCostNodeSelection'))
+        }
+
+        setFloatingTopRowData = rowData => {
+            this.afterGridReady(this.gridParamsFunctionSelector('setFloatingTopRowData'), rowData)
+        }
+
+        setFloatingBottomRowData = () => {
+            this.afterGridReady(this.gridParamsFunctionSelector('setFloatingBottomRowData'))
+        }
+
+        getFloatingTopRowCount = () => {
+            return this.afterGridReady(this.gridParamsFunctionSelector('getFloatingTopRowCount'))
+        }
+
+        getFloatingBottomRowCount = () => {
+            return this.afterGridReady(this.gridParamsFunctionSelector('getFloatingBottomRowCount'))
+        }
+
+        getFloatingTopRow = index => {
+            return this.afterGridReady(this.gridParamsFunctionSelector('getFloatingTopRow'), index)
+        }
+
+        getFloatingBottomRow = index => {
+            return this.afterGridReady(this.gridParamsFunctionSelector('getFloatingBottomRow'), index)
+        }
+
+        refreshView = () => {
+            this.afterGridReady(this.gridParamsFunctionSelector('refreshView'))
+        }
+
+        sortRefreshView = () => {
+            this.afterGridReady(this.gridParamsFunctionSelector('sortRefreshView'))
+        }
+
+        refreshRows = rowNodes => {
+            this.afterGridReady(this.gridParamsFunctionSelector('refreshRows'), rowNodes)
+        }
+
+        refreshCells = (rowNodes, colIds) => {
+            return this.afterGridReady(this.gridParamsFunctionSelector('refreshCells'), rowNodes, colIds)
+        }
+
+        refreshHeader = () => {
+            this.afterGridReady(this.gridParamsFunctionSelector('refreshHeader'))
+        }
+
+        refreshGroupRows = () => {
+            this.afterGridReady(this.gridParamsFunctionSelector('refreshGroupRows'))
+        }
+
+        refreshGroup = () => {
+            this.afterGridReady(this.gridParamsFunctionSelector('refreshGroup'))
+        }
+
+        getModel = () => {
+            return this.afterGridReady(this.gridParamsFunctionSelector('getModel'))
+        }
+
+        onGroupExpandedOrCollapsed = () => {
+            return this.afterGridReady(this.gridParamsFunctionSelector('onGroupExpandedOrCollapsed'))
+        }
+
+        refreshInMemoryRowModel = () => {
+            this.afterGridReady(this.gridParamsFunctionSelector('refreshInMemoryRowModel'))
+        }
+
+        expandAll = () => {
+            this.afterGridReady(this.gridParamsFunctionSelector('expandAll'))
+        }
+
+        collapseAll = () => {
+            this.afterGridReady(this.gridParamsFunctionSelector('collapseAll'))
+        }
+
+        setQuickFilter = quickFilter => {
+            this.afterGridReady(this.gridParamsFunctionSelector('setQuickFilter'), quickFilter)
+        }
+
+        isQuickFilterPresent = () => {
+            return this.afterGridReady(this.gridParamsFunctionSelector('isQuickFilterPresent'))
+        }
+
+        isAdvancedFilterPresent = () => {
+            return this.afterGridReady(this.gridParamsFunctionSelector('isAdvancedFilterPresent'))
+        }
+
+
+        isAnyFilterPresent = () => {
+            return this.afterGridReady(this.gridParamsFunctionSelector('isAnyFilterPresent'))
+        }
+
+        exportDataAsCsv = () => {
+            const { gridParams } = this.state
+
+            this.afterGridReady(this.gridParamsFunctionSelector('exportDataAsCsv'), gridParams)
         }
 
         render() {
